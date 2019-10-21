30c330d2f8bbe89827f0d76302332e9206c8a702
Ajaa44138 <56814033+Ajaa44138@users.noreply.github.com>
2019-10-21 14:00:43 +0800

    albert

--- OOP_project/src/Run.java
+++ OOP_project/src/Run.java
@@ -2,6 +2,7 @@ class Run {
     public static void main(String[] args) {
         System.out.println("something");
 	System.out.println("albert");
+	System.out.println("albert2");
         
     }
     
